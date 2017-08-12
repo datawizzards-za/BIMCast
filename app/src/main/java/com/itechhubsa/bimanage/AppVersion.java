e0d11562c921d1e4d70a1f5786d4901c8444e44b
doctorlloyd <doctorlloyd9@gmail.com>
2017-08-12 08:03:31 -0700

    updated

---
+++ app/src/main/java/com/itechhubsa/bimanage/AppVersion.java
@@ -0,0 +1,13 @@
+package com.itechhubsa.bimanage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AppVersion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.app_version);
+    }
+}
